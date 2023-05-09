23164f017ed2f10057d8b27d45a144785a131801
Rafaela Alves <100208588+ProjetRafaelaAlves1C@users.noreply.github.com>
2023-05-09 12:12:39 -0300

    Add files via upload

---
+++ aula1IW/teste.js
@@ -0,0 +1,25 @@
+
+       //alert("OI RAFAELA")//INTERAÇÃO C USUARIO
+       //confirm("Você é capaz?")//INTERAÇÃO C USUARIO
+       //var nome = prompt("Digite seu nome")//INTERAÇÃO C USUARIO (só aceita string) var nome= declaração variavel
+       //console.log(nome) // ver o valor da variavel (visão dev F12)
+       //document.write(nome)// ver o valor da variavel (visão usuário)
+//---------------------------------------------------------------------------------------------------------------------//
+       //VARIAVEIS
+       //var a = 15// pode ser alterado
+
+       //let é para variaveis locais(específica para uma x coisa)
+       
+       //const a = 10//const é para nunca alterar o valor dela
+//------------------------------------------------------------------------------//
+        //var nome = prompt("Digite seu nome")
+       //alert("Bom dia " + nome)//concatenação simples 
+      //alert(`Bom dia ${nome}`)//interpolação (string junto com variável de uma vez)
+//-----------------------------------------------------------------------------------------//
+//var n1 = parseInt(prompt("Digite número 1"))
+//var n2 = parseint(prompt("Digite o número 2"))
+//var soma = n1+n2
+//document.write(`a soma do ${n1} mais ${n2} é ${soma}`) 
+//console.log("n1 É " + typeof soma)
+
+  
